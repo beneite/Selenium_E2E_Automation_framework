2f2942cec983db323e3aa8b5c12ff75fb4c0c475
Ashish Mishra <asismisr@CTIPL-MAC-348.local>
2024-12-29 12:42:45 +0530

    adding extent report functionality

---
+++ src/main/java/com/asismisr/codeUtils/CommonUtilis.java
@@ -0,0 +1,114 @@
+package com.asismisr.codeUtils;
+
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
+import java.util.Calendar;
+import java.util.Date;
+
+public class CommonUtilis {
+
+    /**
+     * This method will return the date in the specified format w.r.t to the current date
+     * <br> Enter positive integers for future dates </br>
+     * <br> Enter negative integers for past dates </br>
+     * @param dateFormat dateFormat
+     * @param days days
+     * @return
+     */
+    public static String getDate(String dateFormat, int days) {
+
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(new Date());
+        calendar.add(Calendar.DATE, days);
+
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat(dateFormat);
+
+        return simpleDateFormat.format(calendar.getTime());
+    }
+
+    public static String getCurrentDateTime() {
+        LocalDate currentDate = LocalDate.now();
+        LocalTime midnight = LocalTime.of(0, 0, 0);
+        LocalDateTime currentDateTime = LocalDateTime.of(currentDate, midnight);
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        return currentDateTime.format(formatter);
+    }
+
+    public static String getCurrentDateTimeWithDays(int daysToAddOrSubtract) {
+        LocalDate currentDate = LocalDate.now();
+        LocalTime midnight = LocalTime.of(0, 0, 0);
+        LocalDateTime currentDateTime = LocalDateTime.of(currentDate, midnight);
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        LocalDateTime modifiedDateTime;
+        if(daysToAddOrSubtract>0) {
+            modifiedDateTime=currentDateTime.plus(daysToAddOrSubtract, ChronoUnit.DAYS);
+        }
+        else {
+            modifiedDateTime=currentDateTime.minus(daysToAddOrSubtract*-1, ChronoUnit.DAYS);
+        }
+        return modifiedDateTime.format(formatter);
+    }
+
+    public static String getCurrentDateTimeWithDays(int daysToAddOrSubtract, String format) {
+        LocalDate currentDate = LocalDate.now();
+        LocalTime midnight = LocalTime.of(0, 0, 0);
+        LocalDateTime currentDateTime = LocalDateTime.of(currentDate, midnight);
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern(format);
+        LocalDateTime modifiedDateTime;
+        if(daysToAddOrSubtract>0) {
+            modifiedDateTime=currentDateTime.plus(daysToAddOrSubtract, ChronoUnit.DAYS);
+        }
+        else {
+            modifiedDateTime=currentDateTime.minus(daysToAddOrSubtract*-1, ChronoUnit.DAYS);
+        }
+        return modifiedDateTime.format(formatter);
+    }
+
+    /**
+     * method to convert date time from one format to another
+     * @param dateToBeConverted - 2024-06-15T13:58:54.997781
+     * @param format - dd-MM-yyyy HH:mm:ss
+     * @return 15-06-2024 13:58:54
+     */
+    public static String convertDateTimeToAnotherFormat(String dateToBeConverted, String format){
+        // Parse the input date string to a LocalDate object
+        LocalDateTime date = LocalDateTime.parse(dateToBeConverted);
+
+        // Define the desired output format
+        DateTimeFormatter outputFormatter = DateTimeFormatter.ofPattern(format);
+
+        // Format the LocalDate object to the desired output format
+        String formattedDate = date.format(outputFormatter);
+
+        return formattedDate;
+    }
+
+    /**
+     * Returns the current date and time in the specified format.
+     *
+     * @param targetFormat the format in which the current date and time should be returned.
+     *                     For example, "yyyy-MM-dd HH:mm:ss" or "EEEE, MMM dd, yyyy hh:mm a".
+     * @return a string representing the current date and time in the specified format,
+     *         or an error message if the format is invalid.
+     */
+    public static String getCurrentDateTimeInSpecifiedFormat(String targetFormat) {
+        try {
+            // Get the current date and time
+            LocalDateTime currentDateTime = LocalDateTime.now();
+
+            // Create a formatter using the provided format
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern(targetFormat);
+
+            // Format the current date and time
+            return currentDateTime.format(formatter);
+        } catch (IllegalArgumentException e) {
+            // Handle invalid format strings
+            return "Invalid date-time format: " + e.getMessage();
+        }
+    }
+
+}
--- src/test/java/com/asismisr/test/vendorPortal/VendorPortalTest.java
+++ src/test/java/com/asismisr/test/vendorPortal/VendorPortalTest.java
@@ -6,7 +6,10 @@ import com.asismisr.pages.vendorPortal.userLogin.LoginPage;
 import com.asismisr.test.BaseTest;
 import com.asismisr.test.vendorPortal.model.VendorPortalTestData;
 import com.asismisr.constants.Constants;
+import com.asismisr.utils.extentreport.ExtentReportManager;
+import com.asismisr.utils.extentreport.ExtentReportUtils;
 import com.asismisr.utils.jsonparsing.JsonUtil;
+import com.aventstack.extentreports.ExtentTest;
 import org.testng.Assert;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Parameters;
@@ -30,15 +33,27 @@ public final class VendorPortalTest extends BaseTest {
 
     @Test
     public void loginTest(){
+        ExtentReportUtils.extentCreateTest("[Vendor-001] Login Test");
         this.loginPage = new LoginPage();
         loginPage.goTo(Config.getTestProperty(Constants.VENDOR_PORTAL_URL));
+        ExtentReportManager.getExtentTestFromThreadLocal().info("URL Launched");
         Assert.assertTrue(loginPage.isAt());
         loginPage.login(testData.username(), testData.password());
+        ExtentReportManager.getExtentTestFromThreadLocal().pass("[Vendor-001] Test passed");
     }
 
     @Test(dependsOnMethods = "loginTest")
     public void dashboardTest(){
-        loginTest();
+        ExtentReportUtils.extentCreateTest("[Vendor-002] Dashboard Test");
+
+        // login test [Vendor-001] Login Test
+        this.loginPage = new LoginPage();
+        loginPage.goTo(Config.getTestProperty(Constants.VENDOR_PORTAL_URL));
+        ExtentReportManager.getExtentTestFromThreadLocal().info("URL Launched");
+        Assert.assertTrue(loginPage.isAt());
+        loginPage.login(testData.username(), testData.password());
+
+        // dashboard test
         this.dashboardPage = new DashboardPage();
         Assert.assertTrue(dashboardPage.isAt());
 
@@ -51,14 +66,37 @@ public final class VendorPortalTest extends BaseTest {
         // order history search
         dashboardPage.searchOrderHistoryBy(testData.searchKeyword());
         Assert.assertEquals(dashboardPage.getSearchResultsCount(), testData.searchResultsCount());
+        ExtentReportManager.getExtentTestFromThreadLocal().pass("[Vendor-002] Test passed");
     }
 
     @Test(dependsOnMethods = "dashboardTest")
     public void logoutTest(){
-        loginTest();
-        dashboardTest();
+        ExtentReportUtils.extentCreateTest("[Vendor-003] Logout Test");
+
+        // login test [Vendor-001] Login Test
+        this.loginPage = new LoginPage();
+        loginPage.goTo(Config.getTestProperty(Constants.VENDOR_PORTAL_URL));
+        ExtentReportManager.getExtentTestFromThreadLocal().info("URL Launched");
+        Assert.assertTrue(loginPage.isAt());
+        loginPage.login(testData.username(), testData.password());
+
+        // dashboard test [Vendor-002] Dashboard Test
+        this.dashboardPage = new DashboardPage();
+        Assert.assertTrue(dashboardPage.isAt());
+
+        // finance metrics
+        Assert.assertEquals(dashboardPage.getMonthlyEarning(), testData.monthlyEarning());
+        Assert.assertEquals(dashboardPage.getAnnualEarning(), testData.annualEarning());
+        Assert.assertEquals(dashboardPage.getProfitMargin(), testData.profitMargin());
+        Assert.assertEquals(dashboardPage.getAvailableInventory(), testData.availableInventory());
+
+        // order history search
+        dashboardPage.searchOrderHistoryBy(testData.searchKeyword());
+        Assert.assertEquals(dashboardPage.getSearchResultsCount(), testData.searchResultsCount());
+
         dashboardPage.logout();
         Assert.assertTrue(loginPage.isAt());
+        ExtentReportManager.getExtentTestFromThreadLocal().pass("[Vendor-003] Test passed");
     }
 
 }
